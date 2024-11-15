dcdba5a9415d5a28149800a315ffde2db7e2da3e
Varun Tripathi <111790705+VarunTripathi2801@users.noreply.github.com>
2024-11-15 13:06:50 +0530

    Add files via upload

---
+++ Skill_Quiz/DB/project.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2019 at 07:45 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
+(1, 'admin@gmail.com', '12345'),
+(2, 'admin@admin.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`qid`, `ansid`) VALUES
+('55892169bf6a7', '55892169d2efc'),
+('5589216a3646e', '5589216a48722'),
+('558922117fcef', '5589221195248'),
+('55892211e44d5', '55892211f1fa7'),
+('558922894c453', '558922895ea0a'),
+('558922899ccaa', '55892289aa7cf'),
+('558923538f48d', '558923539a46c'),
+('55892353f05c4', '55892354051be'),
+('558973f4389ac', '558973f462e61'),
+('558973f4c46f2', '558973f4d4abe'),
+('558973f51600d', '558973f526fc5'),
+('558973f55d269', '558973f57af07'),
+('558973f5abb1a', '558973f5e764a'),
+('5589751a63091', '5589751a81bf4'),
+('5589751ad32b8', '5589751adbdbd'),
+('5589751b304ef', '5589751b3b04d'),
+('5589751b749c9', '5589751b9a98c'),
+('5ca4862f9e971', '5ca4862fa0a19'),
+('5ca4862fb2128', '5ca4862fb29bb'),
+('5ca4862fb592a', '5ca4862fb6a95'),
+('5ca4862fbaba9', '5ca4862fbb439');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` text NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(500) NOT NULL,
+  `feedback` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `feedback`, `date`, `time`) VALUES
+('5cb192aae77ad', 'Bharat Prakash', 'bharatprakash@gmail.com', 'PHP Question Weight', 'Respected Sir,\r\n\r\nPlease Make sure the Weight of Each Questions, Whatever you have Uploaded on Client panel.', '2019-04-13', '07:41:30am');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `email` varchar(50) NOT NULL,
+  `eid` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
+('aishsiddiqui31@gmail.com', '5589741f9ed52', 4, 5, 3, 2, '2018-04-09 17:38:25'),
+('aishsiddiqui31@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2018-04-10 12:12:41'),
+('ayeshasd@iul.ac.in', '5589741f9ed52', 1, 5, 2, 3, '2018-04-10 12:16:26'),
+('bhabhait1989@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2019-04-03 09:55:10'),
+('bhabhait1989@gmail.com', '5ca4845f5956e', 8, 4, 4, 0, '2019-04-03 10:10:12'),
+('bharatprakash@gmail.com', '5ca4845f5956e', 5, 4, 3, 1, '2019-04-13 07:36:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `qid` varchar(50) NOT NULL,
+  `option` varchar(5000) NOT NULL,
+  `optionid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
+('55892169bf6a7', 'usermod', '55892169d2efc'),
+('55892169bf6a7', 'useradd', '55892169d2f05'),
+('55892169bf6a7', 'useralter', '55892169d2f09'),
+('55892169bf6a7', 'groupmod', '55892169d2f0c'),
+('5589216a3646e', '751', '5589216a48713'),
+('5589216a3646e', '752', '5589216a4871a'),
+('5589216a3646e', '754', '5589216a4871f'),
+('5589216a3646e', '755', '5589216a48722'),
+('558922117fcef', 'echo', '5589221195248'),
+('558922117fcef', 'print', '558922119525a'),
+('558922117fcef', 'printf', '5589221195265'),
+('558922117fcef', 'cout', '5589221195270'),
+('55892211e44d5', 'int a', '55892211f1f97'),
+('55892211e44d5', '$a', '55892211f1fa7'),
+('55892211e44d5', 'long int a', '55892211f1fb4'),
+('55892211e44d5', 'int a$', '55892211f1fbd'),
+('558922894c453', 'cin>>a;', '558922895ea0a'),
+('558922894c453', 'cin<<a;', '558922895ea26'),
+('558922894c453', 'cout>>a;', '558922895ea34'),
+('558922894c453', 'cout<a;', '558922895ea41'),
+('558922899ccaa', 'cout', '55892289aa7cf'),
+('558922899ccaa', 'cin', '55892289aa7df'),
+('558922899ccaa', 'print', '55892289aa7eb'),
+('558922899ccaa', 'printf', '55892289aa7f5'),
+('558923538f48d', '255.0.0.0', '558923539a46c'),
+('558923538f48d', '255.255.255.0', '558923539a480'),
+('558923538f48d', '255.255.0.0', '558923539a48b'),
+('558923538f48d', 'none of these', '558923539a495'),
+('55892353f05c4', '192.168.1.100', '5589235405192'),
+('55892353f05c4', '172.168.16.2', '55892354051a3'),
+('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
+('55892353f05c4', '11.11.11.11', '55892354051be'),
+('558973f4389ac', 'containing root file-system required during bootup', '558973f462e44'),
+('558973f4389ac', ' Contains only scripts to be executed during bootup', '558973f462e56'),
+('558973f4389ac', ' Contains root-file system and drivers required to be preloaded during bootup', '558973f462e61'),
+('558973f4389ac', 'None of the above', '558973f462e6b'),
+('558973f4c46f2', 'Kernel', '558973f4d4abe'),
+('558973f4c46f2', 'Shell', '558973f4d4acf'),
+('558973f4c46f2', 'Commands', '558973f4d4ad9'),
+('558973f4c46f2', 'Script', '558973f4d4ae3'),
+('558973f51600d', 'Boot Loading', '558973f526f9d'),
+('558973f51600d', ' Boot Record', '558973f526fb9'),
+('558973f51600d', ' Boot Strapping', '558973f526fc5'),
+('558973f51600d', ' Booting', '558973f526fce'),
+('558973f55d269', ' Quick boot', '558973f57aef1'),
+('558973f55d269', 'Cold boot', '558973f57af07'),
+('558973f55d269', ' Hot boot', '558973f57af17'),
+('558973f55d269', ' Fast boot', '558973f57af27'),
+('558973f5abb1a', 'bash', '558973f5e7623'),
+('558973f5abb1a', ' Csh', '558973f5e7636'),
+('558973f5abb1a', ' ksh', '558973f5e7640'),
+('558973f5abb1a', ' sh', '558973f5e764a'),
+('5589751a63091', 'q', '5589751a81bd6'),
+('5589751a63091', 'wq', '5589751a81be8'),
+('5589751a63091', ' both (a) and (b)', '5589751a81bf4'),
+('5589751a63091', ' none of the mentioned', '5589751a81bfd'),
+('5589751ad32b8', ' moves screen down one page', '5589751adbdbd'),
+('5589751ad32b8', 'moves screen up one page', '5589751adbdce'),
+('5589751ad32b8', 'moves screen up one line', '5589751adbdd8'),
+('5589751ad32b8', ' moves screen down one line', '5589751adbde2'),
+('5589751b304ef', ' yy', '5589751b3b04d'),
+('5589751b304ef', 'yw', '5589751b3b05e'),
+('5589751b304ef', 'yc', '5589751b3b069'),
+('5589751b304ef', ' none of the mentioned', '5589751b3b073'),
+('5589751b749c9', 'X', '5589751b9a98c'),
+('5589751b749c9', 'x', '5589751b9a9a5'),
+('5589751b749c9', 'D', '5589751b9a9b7'),
+('5589751b749c9', 'd', '5589751b9a9c9'),
+('5589751bd02ec', 'autoindentation is not possible in vi editor', '5589751bdadaa'),
+('5ca4862f9e971', '1 1', '5ca4862fa0a07'),
+('5ca4862f9e971', '1 0', '5ca4862fa0a10'),
+('5ca4862f9e971', '0 1', '5ca4862fa0a15'),
+('5ca4862f9e971', '0 0', '5ca4862fa0a19'),
+('5ca4862fb2128', 'Compiler Error', '5ca4862fb29bb'),
+('5ca4862fb2128', 'Run-time Error', '5ca4862fb29c1'),
+('5ca4862fb2128', 'Syntax Error', '5ca4862fb29c4'),
+('5ca4862fb2128', 'Logical Error', '5ca4862fb29c8'),
+('5ca4862fb592a', '3', '5ca4862fb6a86'),
+('5ca4862fb592a', '2', '5ca4862fb6a8e'),
+('5ca4862fb592a', '1', '5ca4862fb6a91'),
+('5ca4862fb592a', '4', '5ca4862fb6a95'),
+('5ca4862fbaba9', '4', '5ca4862fbb42c'),
+('5ca4862fbaba9', '3', '5ca4862fbb432'),
+('5ca4862fbaba9', '2', '5ca4862fbb435'),
+('5ca4862fbaba9', '1', '5ca4862fbb439');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
+('558920ff906b8', '55892169bf6a7', 'what is command for changing user information??', 4, 1),
+('558920ff906b8', '5589216a3646e', 'what is permission for view only for other??', 4, 2),
+('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
+('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
+('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
+('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
+('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
+('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2),
+('55897338a6659', '558973f4389ac', 'On Linux, initrd is a file', 4, 1),
+('55897338a6659', '558973f4c46f2', 'Which is loaded into memory when system is booted?', 4, 2),
+('55897338a6659', '558973f51600d', ' The process of starting up a computer is known as', 4, 3),
+('55897338a6659', '558973f55d269', ' Bootstrapping is also known as', 4, 4),
+('55897338a6659', '558973f5abb1a', 'The shell used for Single user mode shell is:', 4, 5),
+('5589741f9ed52', '5589751a63091', ' Which command is used to close the vi editor?', 4, 1),
+('5589741f9ed52', '5589751ad32b8', ' In vi editor, the key combination CTRL+f', 4, 2),
+('5589741f9ed52', '5589751b304ef', ' Which vi editor command copies the current line of the file?', 4, 3),
+('5589741f9ed52', '5589751b749c9', ' Which command is used to delete the character before the cursor location in vi editor?', 4, 4),
+('5589741f9ed52', '5589751bd02ec', ' Which one of the following statement is true?', 4, 5),
+('5ca4845f5956e', '5ca4862f9e971', '// filename Main.java \r\nclass Test { \r\n	protected int x, y; \r\n} \r\n\r\nclass Main { \r\n	public static void main(String args[]) { \r\n		Test t = new Test(); \r\n		System.out.println(t.x + " " + t.y); \r\n	} \r\n} \r\n', 4, 1),
+('5ca4845f5956e', '5ca4862fb2128', '// filename Test.java \r\nclass Test { \r\n	public static void main(String[] args) { \r\n		for(int i = 0; 1; i++) { \r\n			System.out.println("Hello"); \r\n			break; \r\n		} \r\n	} \r\n} \r\n', 4, 2),
+('5ca4845f5956e', '5ca4862fb592a', '// filename Main.java \r\nclass Main { \r\n	public static void main(String args[]) { \r\n		System.out.println(fun()); \r\n	} \r\n	static int fun() { \r\n		return 20; \r\n	} \r\n} \r\n', 4, 3),
+('5ca4845f5956e', '5ca4862fbaba9', 'class Test { \r\nprivate static int x; \r\npublic static void main(String args[]) { \r\n	System.out.println(fun()); \r\n} \r\nstatic int fun() { \r\n	return ++x; \r\n} \r\n} \r\n', 4, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `intro` text NOT NULL,
+  `tag` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
+('558920ff906b8', 'Linux : File Managment', 2, 1, 2, 5, '', 'linux', '2015-06-23 09:03:59'),
+('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2015-06-23 09:06:12'),
+('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2015-06-23 09:09:03'),
+('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2015-06-23 09:12:12'),
+('55897338a6659', 'Linux:startup', 2, 1, 5, 10, '', 'linux', '2015-06-23 14:54:48'),
+('5589741f9ed52', 'Linux :vi Editor', 2, 1, 5, 10, '', 'linux', '2015-06-23 14:58:39'),
+('5ca4845f5956e', 'Java (j2se)', 2, 1, 4, 5, '', 'Core Java', '2019-04-03 10:01:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `email` varchar(50) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`email`, `score`, `time`) VALUES
+('aishsiddiqui31@gmail.com', 4, '2018-04-10 12:12:41'),
+('anshika.singh8545@gmail.com', 27, '2018-04-09 18:08:02'),
+('aishsiddiqui31@gmail.com', 4, '2018-04-10 12:12:41'),
+('bhabhait1989@gmail.com', 12, '2019-04-03 10:10:12'),
+('ayeshasd@iul.ac.in', 1, '2018-04-10 12:16:26'),
+('bharatprakash@gmail.com', 5, '2019-04-13 07:36:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mob` bigint(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Anu Singh', 'F', 'Integral University', 'anu.singh8545@gmail.com', 8005143188, '827ccb0eea8a706c4c34a16891f84e7b'),
+('Nasri Bhai', 'M', 'Integral University', 'bhabhait1989@gmail.com', 9807383940, '827ccb0eea8a706c4c34a16891f84e7b'),
+('Bharat Prakash', 'M', 'BERI, Bhopal, M.P', 'bharatprakash@gmail.com', 9807564321, '827ccb0eea8a706c4c34a16891f84e7b'),
+('Priya Patel', 'F', 'Integral University', 'ppatel@iul.ac.in', 8787965410, 'e10adc3949ba59abbe56e057f20f883e'),
+('Ruhi Siddiqui', 'F', 'Integral University', 'ruhisiddiqui31@gmail.com', 8318535520, 'e10adc3949ba59abbe56e057f20f883e');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
